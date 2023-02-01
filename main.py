9c6d5ca08b0dc6d8bccd2c58c93736db242dae39
Shivam <shivam.362000.1@gmail.com>
2023-02-02 01:30:56 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,120 @@
+import os
+from dotenv import load_dotenv
+import telebot
+import timetable
+
+load_dotenv()
+
+API_KEY = os.environ.get('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+
+@bot.message_handler(commands=['getcore'])
+def get_core(message):
+    sent_msg = bot.send_message(message.chat.id, 'Please enter your section followed by the day (e.g. CSE-1 MON): ')
+    bot.register_next_step_handler(sent_msg, return_core_time_table)
+
+
+def return_core_time_table(message):
+    data = message.text
+    keyword_list = data.split()
+    try:
+        res_dict = timetable.get_core_time_table(keyword_list[0], keyword_list[1])
+        print(res_dict)
+        res = core_dict_to_table(res_dict)
+    except Exception as ex:
+        print(ex)
+        res = 'Please enter the command in the right format.'
+
+    bot.send_message(message.chat.id, res)
+
+
+def core_dict_to_table(res_dict):
+    res = '''
+     {:<10}: {:<10}
+     {:<10}: {:<10} 
+     {:<10}: {:<10}
+     {:<10}: {:<10} 
+     {:<10}: {:<10} 
+     {:<10}: {:<10} 
+     {:<10}: {:<10} 
+     {:<10}: {:<10} 
+     {:<10}: {:<10}
+     '''.format(
+        'DAY', res_dict.get('DAY'),
+        'ROOM1', res_dict.get('ROOM1'),
+        '8-9', res_dict.get('8-9'),
+        '9-10', res_dict.get('9-10'),
+        '10-11', res_dict.get('10-11'),
+        'ROOM2', res_dict.get('ROOM2'),
+        '11-12', res_dict.get('11-12'),
+        '12-1', res_dict.get('12-1'),
+        '1-2', res_dict.get('1-2')
+    )
+
+    print(res)
+    return res
+
+
+# @bot.message_handler(commands=['getEle'])
+# def get_elective(message):
+#     sent_msg = bot.send_message(message.chat.id,
+#                                 'Please enter your elective code, section and day (e.g. ML_CSE-1 MON): ')
+#     bot.register_next_step_handler(sent_msg, return_elective_time_table)
+
+
+# def return_elective_time_table(message):
+#     data = message.text
+#     keyword_list = data.split()
+#     res_dict = timetable.get_ele_time_table(keyword_list[0], keyword_list[1])
+#     print(res_dict)
+#     res = ele_dict_to_table(res_dict)
+#     bot.send_message(message.chat.id, res)
+
+
+# def ele_dict_to_table(res_dict):
+    # res = '''
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      {:<10}: {:<10}
+    #      '''.format(
+    #     'DAY', res_dict.get('DAY'),
+    #     'ROOM1', res_dict.get('ROOM1'),
+    #     '11-12', res_dict.get('11-12'),
+    #     'ROOM2', res_dict.get('ROOM2'),
+    #     '12-1', res_dict.get('12-1'),
+    #     'ROOM3', res_dict.get('ROOM3'),
+    #     '1-2', res_dict.get('10-11'),
+    #     'ROOM4', res_dict.get('ROOM4'),
+    #     '3-4', res_dict.get('3-4'),
+    #     'ROOM5', res_dict.get('ROOM5'),
+    #     '4-5', res_dict.get('4-5')
+    # )
+
+    # res = f'''
+    #     DAY: {res_dict.get('DAY')}
+    #     ROOM1: {res_dict.get('ROOM1')}
+    #     11-12: {res_dict.get('11-12')}
+    #     ROOM2: {res_dict.get('ROOM2')}
+    #     12-1: {res_dict.get('12-1')}
+    #     ROOM3: {res_dict.get('ROOM3')}
+    #     1-2: {res_dict.get('10-11')}
+    #     ROOM4: {res_dict.get('ROOM4')}
+    #     3-4: {res_dict.get('3-4')}
+    #     ROOM5: {res_dict.get('ROOM5')}
+    #     4-5: {res_dict.get('4-5')}
+    #     '''
+    #
+    # print(res)
+    # return res
+
+
+bot.polling()
---
+++ timetable.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+
+def get_core_time_table(section, day):
+    data = pd.read_csv('sem-6-time-table.csv')
+    # filter_by_section = data.groupby('Section')
+    # sec_group = filter_by_section.get_group(section)
+
+    sec_group = filter_by_section(data, section)
+
+    base = sec_group.index[0]
+
+    day_index = {
+        'MON': base + 0,
+        'TUE': base + 1,
+        'WED': base + 2,
+        'THU': base + 3,
+        'FRI': base + 4,
+    }
+
+    return {
+        'DAY': sec_group['DAY'][day_index.get(day)],
+        'ROOM1': sec_group['ROOM1'][day_index.get(day)],
+        '8-9': sec_group['8-9'][day_index.get(day)],
+        '9-10': sec_group['9-10'][day_index.get(day)],
+        '10-11': sec_group['10-11'][day_index.get(day)],
+        'ROOM2': sec_group['ROOM2'][day_index.get(day)],
+        '11-12': sec_group['11-12'][day_index.get(day)],
+        '12-1': sec_group['12-1'][day_index.get(day)],
+        '1-2': sec_group['1-2'][day_index.get(day)]
+    }
+
+
+# def get_ele_time_table(section, day):
+#     data = pd.read_csv('sem-6-elective-time-table.csv')
+#     # filter_by_section = data.groupby('Section')
+#     # sec_group = filter_by_section.get_group(section)
+#
+#     sec_group = filter_by_section(data, section)
+#
+#     base = sec_group.index[0]
+#
+#     day_index = {
+#         'MON': base + 0,
+#         'TUE': base + 1,
+#         'WED': base + 2,
+#         'THU': base + 3,
+#         'FRI': base + 4,
+#     }
+#
+#     return {
+#         'DAY': sec_group['DAY'][day_index.get(day)],
+#         'ROOM1': sec_group['ROOM1'][day_index.get(day)],
+#         '11-12': sec_group['11-12'][day_index.get(day)],
+#         'ROOM2': sec_group['ROOM2'][day_index.get(day)],
+#         '12-1': sec_group['12-1'][day_index.get(day)],
+#         'ROOM3': sec_group['ROOM3'][day_index.get(day)],
+#         '1-2': sec_group['1-2'][day_index.get(day)],
+#         'ROOM4': sec_group['ROOM4'][day_index.get(day)],
+#         '3-4': sec_group['3-4'][day_index.get(day)],
+#         'ROOM5': sec_group['ROOM5'][day_index.get(day)],
+#         '4-5': sec_group['4-5'][day_index.get(day)]
+#     }
+
+
+def filter_by_section(data, section):
+    return data.groupby('Section').get_group(section)
